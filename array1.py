519d78b5dbd5bd9f403605649b2a70006f386552
Shubhi Agarwal <shubhi1488@gmail.com>
2022-07-15 20:10:06 +0530

    Create array1.py

---
+++ array1.py
@@ -0,0 +1,10 @@
+import numpy as np;
+#creating single dimensional array-
+arr=np.array([1,2,3])
+print("rank 1 dimensional array:\n",arr)
+#creating two dimensional array
+arr=np.array([[1,2,3],[4,5,6]])
+print("rank 2 dimensional array:\n",arr)
+#creating array from tuple
+arr=np.array((1,2,3))
+print("array from tuple is:\n",arr)
